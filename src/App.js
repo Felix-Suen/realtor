afa7b235670a43341f829e1b4595c00416fd86ac
Felix-Suen <felix.suen2000@gmail.com>
2020-12-03 15:53:07 -0800

    clean up react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Test from './Test';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Test />
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>Hello world</div>
+            <Test />
+        </div>
+    );
 }
 
 export default App;
