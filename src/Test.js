834244b0279aed198a36bdd6f2551ef57c39a58e
Felix-Suen <felix.suen2000@gmail.com>
2020-12-05 10:54:05 -0800

    added proxy

--- src/Test.js
+++ src/Test.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import qs from 'querystring';
+import './App.css';
 
 const Test = () => {
     const [errors, setErrors] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [content, setContent] = useState([]);
 
-    // const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
     const url = 'https://api.realtor.ca/Listing.svc/PropertySearch_Post';
     const options = {
-
         LongitudeMin: -79.6758985519409,
         LongitudeMax: -79.6079635620117,
         LatitudeMin: 43.57601549736786,
@@ -30,7 +30,7 @@ const Test = () => {
 
     useEffect(async () => {
         const res = await axios.post(
-            url,
+            proxyurl + url,
             qs.stringify(options),
             config
         );
@@ -54,12 +54,13 @@ const Test = () => {
             <h1>Info</h1>
 
             <table>
-                <tr style={{ textAlign: 'left' }}><th>Address</th><th>Price</th><th>Type</th><th># Bedroom</th></tr>
-                {content.map((result) => <tr>
-                    <td style={{ paddingRight: '20px' }}>{result.Property.Address.AddressText}</td>
-                    <td style={{ paddingRight: '20px' }}>{result.Property.Price}</td>
-                    <td style={{ paddingRight: '20px' }}>{result.Property.Type}</td>
-                    <td style={{ paddingRight: '20px' }}>{result.Building.Bedrooms}</td>
+                <tr style={{ textAlign: 'left' }}><th>Entry</th><th>Address</th><th>Price</th><th>Type</th><th># Bedroom</th></tr>
+                {content.map((result, index) => <tr>
+                    <td>{index + 1}</td>
+                    <td>{result.Property.Address.AddressText}</td>
+                    <td>{result.Property.Price}</td>
+                    <td>{result.Property.Type}</td>
+                    <td>{result.Building.Bedrooms}</td>
                     </tr>)}
             </table>
             
