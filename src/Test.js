9d2231967fda6772cc94bc984ccfe4818383f72a
Felix-Suen <felix.suen2000@gmail.com>
2020-12-04 18:10:48 -0800

    loading state

--- src/Test.js
+++ src/Test.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 
 const Test = () => {
     const [errors, setErrors] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     const proxyurl = 'https://cors-anywhere.herokuapp.com/';
     const url = 'https://api.realtor.ca/Listing.svc/PropertySearch_Post';
     const options = {
-        CultureId: 0,
+        CultureId: 1,
         ApplicationId: 37,
         PropertySearchTypeId: 1,
         LongitudeMin: -79.6758985519409,
@@ -33,15 +34,23 @@ const Test = () => {
 
         console.log(res.data);
         setErrors(res.data.ErrorCode);
+        setIsLoading(false);
     }, []);
 
     return (
-        <ul>
-            <li>{errors.Description}</li>
-            <li>{errors.Id}</li>
-            <li>{errors.ProductName}</li>
-        </ul>
-        );
+        <div style={{ display: 'table', margin: '0 auto', padding: '20px' }}>
+            {isLoading ? (
+                <div>Takes a long ass time to load...</div>
+            ) : (
+                <div>loaded</div>
+            )}
+            <ul>
+                <li>{errors.Description}</li>
+                <li>{errors.Id}</li>
+                <li>{errors.ProductName}</li>
+            </ul>
+        </div>
+    );
 };
 
 export default Test;
