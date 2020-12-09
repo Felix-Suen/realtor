319d20bc3a8d63cf9dc7a9cf072bed1662621789
Felix-Suen <felix.suen2000@gmail.com>
2020-12-09 13:22:05 -0800

    useEffect changed and map viewport

--- src/Test.js
+++ src/Test.js
@@ -9,12 +9,10 @@ const Test = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [content, setContent] = useState([]);
 
-
-
     const [coords, setCoords] = useState({
         Longitude: -79.61436356,
         Latitude: 43.60015014,
-    })
+    });
 
     // default parameters to pass in
     const [options, setOptions] = useState({
@@ -30,7 +28,7 @@ const Test = () => {
     });
 
     const [address, setAddress] = useState({
-      Address: '216 Canyon Hill',
+        Address: '216 Canyon Hill',
     });
 
     // const proxy = 'https://cors-anywhere.herokuapp.com/';
@@ -40,8 +38,6 @@ const Test = () => {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
     };
-
-
 
     // function that calls the api
     async function fetchData(opts) {
@@ -54,87 +50,84 @@ const Test = () => {
         setContent(res.data.Results);
     }
 
-    async function findGeoCode() {
-      // const addressConfig = {
-      //   url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
-      // }
-
-      const res = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address.Address) + '.json?access_token=pk.eyJ1IjoiZmVuZy1ndW8iLCJhIjoiY2tpZzZlbDR0MGNpZzJxcXBodWZ3b3M3cSJ9.SpRJgUpSDBkD_V29dUtpLg',{ params: { limit: 1 } });
-      console.log(res.data);
-      console.log(res.data.features[0].geometry.coordinates);
-      let long = res.data.features[0].geometry.coordinates[0];
-      let lat = res.data.features[0].geometry.coordinates[1]
-      setCoords(prevState => ({
-          Longitude: long,
-          Latitude: lat
-      }));
-      setOptions(prevState => ({
-          ...prevState,
-          LongitudeMax: long + 0.0064,
-          LongitudeMin: long - 0.0064,
-          LatitudeMax: lat + 0.0021,
-          LatitudeMin: lat - 0.0021,
-      }));
-      // console.log(coords);
-      // console.log(options);
-
-
+    async function findGeoCode(address) {
+        const res = await axios.get(
+            'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+                encodeURIComponent(address) +
+                '.json?access_token=pk.eyJ1IjoiZmVuZy1ndW8iLCJhIjoiY2tpZzZlbDR0MGNpZzJxcXBodWZ3b3M3cSJ9.SpRJgUpSDBkD_V29dUtpLg',
+            { params: { limit: 1 } }
+        );
+        console.log(res.data);
+        console.log(res.data.features[0].geometry.coordinates);
+        let long = res.data.features[0].geometry.coordinates[0];
+        let lat = res.data.features[0].geometry.coordinates[1];
+        setCoords((prevState) => ({
+            ...prevState,
+            Longitude: long,
+            Latitude: lat,
+        }));
+        setOptions((prevState) => ({
+            ...prevState,
+            LongitudeMax: long + 0.0064,
+            LongitudeMin: long - 0.0064,
+            LatitudeMax: lat + 0.0021,
+            LatitudeMin: lat - 0.0021,
+        }));
     }
 
     // update the data whenever a parameter gets changed
     useEffect(async () => {
-        findGeoCode();
         fetchData(options);
     }, [options]);
 
     // Filter that changes parameters
-    const onChange = e => {
+    const onChange = (e) => {
         const { name, value } = e.target;
-        setOptions(prevState => ({
+        setOptions((prevState) => ({
             ...prevState,
-            [name]: value
+            [name]: value,
         }));
     };
 
-    const onChangeLong = e => {
+    const onChangeLong = (e) => {
         const { value } = e.target;
-        setCoords(prevState => ({
+        setCoords((prevState) => ({
             ...prevState,
             Longitude: value,
         }));
-        setOptions(prevState => ({
+        setOptions((prevState) => ({
             ...prevState,
             LongitudeMax: parseFloat(value + 0.0064),
             LongitudeMin: value - 0.0064,
         }));
-    }
+    };
 
-    const onChangeLat = e => {
+    const onChangeLat = (e) => {
         const { value } = e.target;
-        setCoords(prevState => ({
+        setCoords((prevState) => ({
             ...prevState,
             Latitude: value,
         }));
-        setOptions(prevState => ({
+        setOptions((prevState) => ({
             ...prevState,
             LatitudeMax: parseFloat(value + 0.0021),
             LatitudeMin: value - 0.0021,
         }));
-    }
+    };
 
-    const onChangeAddress = e => {
-      const {value} = e.target;
-      setAddress(prevState => ({
-          ...prevState,
-          Address: value,
-      }));
-    }
+    const onChangeAddress = (e) => {
+        const { value } = e.target;
+        setAddress((prevState) => ({
+            ...prevState,
+            Address: value,
+        }));
+        findGeoCode(address.Address);
+    };
 
     // const onSubmit = e => {
     //     e.preventDefault();
     //     fetchData(options);
     // }
-
 
     // <label>Address: </label>
     // <input
@@ -169,62 +162,58 @@ const Test = () => {
                 <div>
                     <div>loaded</div>
                     {errors.Id} {errors.Description}
-
                     <h1>Info</h1>
-
                     <form>
-
-
-                    <label>Address: </label>
-                    <input
-                      value={address.Address}
-                      type='text'
-                      onChange={onChangeAddress}
-                      name='Address'
-                    />
-                    <label>Actual Longitude: </label>
+                        <label>Address: </label>
+                        <input
+                            value={address.Address}
+                            type="text"
+                            onChange={onChangeAddress}
+                            name="Address"
+                        />
+                        <label>Actual Longitude: </label>
                         <input
                             value={coords.Longitude}
-                            type='number'
+                            type="number"
                             onChange={onChangeLong}
                             name="Longitude"
-                        />{" "}
+                        />{' '}
                         <label>Actual Latitude: </label>
                         <input
                             value={coords.Latitude}
-                            type='number'
+                            type="number"
                             onChange={onChangeLat}
                             name="Latitude"
-                        /><br /><br />
-
+                        />
+                        <br />
+                        <br />
                         <label>Price Min: </label>
                         <input
                             value={options.PriceMin}
-                            type='number'
+                            type="number"
                             onChange={onChange}
                             name="PriceMin"
-                        />{" "}
+                        />{' '}
                         <label>Price Max: </label>
                         <input
                             value={options.PriceMax}
-                            type='number'
+                            type="number"
                             onChange={onChange}
                             name="PriceMax"
-                        />{" "}
+                        />{' '}
                         <label>Number of Records: </label>
                         <input
                             value={options.RecordsPerPage}
-                            type='number'
+                            type="number"
                             onChange={onChange}
                             name="RecordsPerPage"
                         />
-                        <br /><br />
-
-                        <Map content={content} />
-
-                        <br /><br />
+                        <br />
+                        <br />
+                        <Map content={content} coords={coords}/>
+                        <br />
+                        <br />
                     </form>
-
                     <table>
                         <tr style={{ textAlign: 'left' }}>
                             <th>Entry</th>
